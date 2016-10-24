2a959ee5c16f699a78cbb979e5007fa0d7e4ab0b
Ignacio Lopez Penalver <nacholp@alpha09.aulas.gsyc.urjc.es>
2016-10-24 13:17:38 +0200

    punto6

--- server.py
+++ server.py
@@ -8,20 +8,27 @@ import socketserver
 import sys
 
 
-class EchoHandler(socketserver.DatagramRequestHandler):
+class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
 
     def handle(self):
+        informacion = {}
         self.wfile.write(b"Hemos recibido tu peticion")
         for line in self.rfile:
+            print("Direccion IP y Puerto del cliente: ", self.client_address)
             print("El cliente nos manda ", line.decode('utf-8'))
-            print("Ip y puerto del cliente", self.client_address)
-
+            if line.decode('utf-8')[:8] == 'REGISTER': 
+                informacion[line.decode('utf-8')[14:-9]]= self.client_address[0]
+                print("El cliente nos manda ", line.decode('utf-8'))
+                print('SIP/2.0 200 OK\r\n\r\n')
+                print("diccionario", informacion)
+  
 if __name__ == "__main__":
-    serv = socketserver.UDPServer(('', int(sys.argv[1])), EchoHandler)
-    print("Lanzando servidor UDP de eco...")
+    serv = socketserver.UDPServer(('', int(sys.argv[1])), SIPRegisterHandler)
+    print("Lanzando servidor UDP de SIP")
+    
     try:
         serv.serve_forever()
     except KeyboardInterrupt:
